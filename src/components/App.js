c69cf6b0143f3d9ef21b069c6b87b990baba61c0
Andrius Kvedaras <ikandrius@gmail.com>
2019-12-29 15:22:06 +0200

    Lazy load route components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
+import { UserProvider } from "../context/user-context";
 import Header from "./Header";
 import Home from "./Home";
-import Snippets from "./Snippets";
-import RecentSnippets from "./RecentSnippets";
-import View from "./View";
-import Form from "./Form";
-import UserForms from "./UserForms";
-import NoMatch from "./NoMatch";
-import UserSettings from "./UserSettings";
+import PageSpinner from "./PageSpinner";
 
-import { UserProvider } from "../context/user-context";
+const Snippets = lazy(() => import("./Snippets"));
+const RecentSnippets = lazy(() => import("./RecentSnippets"));
+const View = lazy(() => import("./View"));
+const Form = lazy(() => import("./Form"));
+const UserForms = lazy(() => import("./UserForms"));
+const UserSettings = lazy(() => import("./UserSettings"));
+const NoMatch = lazy(() => import("./NoMatch"));
 
 export default function App() {
   return (
@@ -18,21 +19,23 @@ export default function App() {
       <UserProvider>
         <Header/>
         <main>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/snippets" exact component={Snippets} key="local" />
-            <Route path="/snippets/create" component={Form} key="create" />
-            <Route path="/snippets/recent" component={RecentSnippets} />
-            <Route path="/snippets/:id" exact component={View} key="local" />
-            <Route path="/snippets/:id/edit" component={Form} key="edit" />
-            <Route path="/users/:username" exact component={Snippets} key="remote" />
-            <Route path="/users/:username/:snippetId" exact component={View} key="remote" />
-            <Route path="/users/:username/:snippetId/edit" component={Form} key="remote" />
-            <Route path="/login" component={UserForms} key="login" />
-            <Route path="/register" component={UserForms} key="register" />
-            <Route path="/settings" component={UserSettings} />
-            <Route component={NoMatch} />
-          </Switch>
+          <Suspense fallback={<PageSpinner/>}>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/snippets" exact component={Snippets} key="local" />
+              <Route path="/snippets/create" component={Form} key="create" />
+              <Route path="/snippets/recent" component={RecentSnippets} />
+              <Route path="/snippets/:id" exact component={View} key="local" />
+              <Route path="/snippets/:id/edit" component={Form} key="edit" />
+              <Route path="/users/:username" exact component={Snippets} key="remote" />
+              <Route path="/users/:username/:snippetId" exact component={View} key="remote" />
+              <Route path="/users/:username/:snippetId/edit" component={Form} key="remote" />
+              <Route path="/login" component={UserForms} key="login" />
+              <Route path="/register" component={UserForms} key="register" />
+              <Route path="/settings" component={UserSettings} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Suspense>
         </main>
       </UserProvider>
     </HashRouter>
--- src/components/RecentSnippets/SnippetDropdown/SnippetDropdown.js
+++ src/components/RecentSnippets/SnippetDropdown/SnippetDropdown.js
@@ -6,12 +6,12 @@ export default function SnippetDropdown({ snippet, forkSnippet, toggleSnippetFav
   return (
     <Dropdown
       toggle={{ content: <Icon name="dots" />, title: "Toggle action menu", className: "btn icon-btn" }}
-      body={{ className: "snippet-dropdown" }}>
-      <button className="btn icon-text-btn dropdown-btn snippet-dropdown-btn" onClick={() => forkSnippet(snippet)}>
+      body={{ className: "recent-snippet-dropdown" }}>
+      <button className="btn icon-text-btn dropdown-btn recent-snippet-dropdown-btn" onClick={() => forkSnippet(snippet)}>
         <Icon name="fork" />
         <span>Fork</span>
       </button>
-      <button className="btn icon-text-btn dropdown-btn snippet-dropdown-btn"
+      <button className="btn icon-text-btn dropdown-btn recent-snippet-dropdown-btn"
         onClick={() => toggleSnippetFavoriteStatus(snippet)}>
         <Icon name="star" />
         <span>Favorite</span>
